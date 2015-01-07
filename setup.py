7c5708c3255e41977e83e598c0f3b55ede675aa0
Justin Karneges <justin@affinix.com>
2015-01-06 18:03:35 -0800

    update version

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
 name='django-grip',
-version='1.2.0',
+version='1.2.1',
 description='Django GRIP library',
 author='Justin Karneges',
 author_email='justin@fanout.io',
 url='https://github.com/fanout/django-grip',
 license='MIT',
 py_modules=['django_grip'],
-install_requires=['pubcontrol>=1.0.4', 'gripcontrol>=1.0.4'],
+install_requires=['pubcontrol>=2.0.0', 'gripcontrol>=2.0.0'],
 classifiers=[
 	'Topic :: Utilities',
 	'License :: OSI Approved :: MIT License'
