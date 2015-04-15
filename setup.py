0607ca279e063b48e749fac07c065ecd44a558b2
Justin Karneges <justin@affinix.com>
2015-04-15 14:25:32 -0700

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
 name='django-grip',
-version='1.6.0',
+version='1.6.1',
 description='Django GRIP library',
 author='Justin Karneges',
 author_email='justin@fanout.io',
