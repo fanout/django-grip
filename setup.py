3ff83e976473a3bb0a7c34dc504990cac4eed83b
Justin Karneges <justin@karneges.com>
2019-10-26 10:57:28 -0700

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
 name='django-grip',
-version='2.0.1',
+version='2.0.2',
 description='Django GRIP library',
 author='Justin Karneges',
 author_email='justin@fanout.io',
