0334e39f6690bb1a693e0079f00cf7387fbd85d4
Justin Karneges <justin@karneges.com>
2019-12-21 09:33:17 -0800

    bump required pubcontrol and gripcontrol versions

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ author_email='justin@fanout.io',
 url='https://github.com/fanout/django-grip',
 license='MIT',
 py_modules=['django_grip'],
-install_requires=['Django>=1.9', 'pubcontrol>=2.4.1,<3', 'gripcontrol>=3.3.0,<4', 'Werkzeug>=0.12,<1', 'six>=1.10,<2'],
+install_requires=['Django>=1.9', 'pubcontrol>=3.0,<4', 'gripcontrol>=4.0,<5', 'Werkzeug>=0.12,<1', 'six>=1.10,<2'],
 classifiers=[
 	'Topic :: Utilities',
 	'License :: OSI Approved :: MIT License'
